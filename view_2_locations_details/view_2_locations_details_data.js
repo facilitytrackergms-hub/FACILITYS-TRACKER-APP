3d85aef4c83ff4c4eb7f0e7c03a656936254db9f
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-16 18:50:25 -0400

    Update view_2_locations_details_data.js

--- view_2_locations_details/view_2_locations_details_data.js
+++ view_2_locations_details/view_2_locations_details_data.js
@@ -2,7 +2,23 @@
    PURPOSE: Data fetching for Location Details
    LOCATION: /FACILITYS-TRACKER-APP/view_2_locations_details/view_2_locations_details_data.js
    ================================================================ */
-
+/* ================================================================
+   PURPOSE: Data fetching for Locations
+   LOCATION: /FACILITYS-TRACKER-APP/view_1_locations/view_1_locations_data.js
+   ================================================================ */
+import { supabase } from '../00_global_engine/supabaseClient.js';
+export async function fetchLocations() {
+    const { data, error } = await supabase
+        .from('locations')
+        .select('*')
+        .order('number_name', { ascending: true });
+    
+    if (error) {
+        console.error("Error fetching locations:", error);
+        return [];
+    }
+    return data;
+}
 export async function fetchLocationDetails(id) {
     const { data, error } = await supabase
         .from('locations')
