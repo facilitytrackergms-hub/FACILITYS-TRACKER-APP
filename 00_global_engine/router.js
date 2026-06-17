aeebf54cd2fc3ee5dc41098fe76bd88c8c3df586
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-16 21:14:55 -0400

    Add renderContacts function to navigation

--- 00_global_engine/router.js
+++ 00_global_engine/router.js
@@ -5,15 +5,22 @@
 
 import { renderLocations } from '../view_1_locations/view_1_locations_grid.js';
 import { renderDetails } from '../view_2_locations_details/view_2_locations_details_grid.js';
+import { renderContacts } from '../view_3_contacts/view_3_contacts_grid.js';
 
 window.navigateTo = (view, context) => {
     switch (view) {
         case 'locations':
             renderLocations();
             break;
+
         case 'view_2_locations_details':
             renderDetails(context);
             break;
+
+        case 'view_3_contacts':
+            renderContacts(context);
+            break;
+
         default:
             renderLocations();
     }
