581d3ae7ea38f3456b46f6963f88f843152c737c
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-21 19:33:51 +0000

    Update materials_grid.js

--- facilities_views/materials/materials_grid.js
+++ facilities_views/materials/materials_grid.js
@@ -1,10 +1,11 @@
 /*================================================================
 SYSTEM: Facility Tracker Modular View System
-PURPOSE: Materials view blank shell
+PURPOSE: Materials view grid calls blank card component
 LOCATION: /facilities_views/materials/materials_grid.js
-VERSION: v2026_06_21_materials_blank_shell
+VERSION: v2026_06_21_grid_calls_blank_card_component
 UPDATED: 2026-06-21
 ================================================================*/
+
 import { renderMaterialsBlankCard } from './materials_blank_card_component.js';
 
 export async function renderMaterialsGrid(containerId, context = {}) {
@@ -12,6 +13,8 @@ export async function renderMaterialsGrid(containerId, context = {}) {
     if (!container) return;
 
     container.innerHTML = `
-        <div id="materials-view-root"></div>
+        <div id="materials-view-root">
+            ${renderMaterialsBlankCard()}
+        </div>
     `;
 }
